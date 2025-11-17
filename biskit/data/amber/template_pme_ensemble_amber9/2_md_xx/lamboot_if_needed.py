fadaf450e7b36ed3c51e50b8c1f66ccdf535b16a
Ryan Stedman <ryangstedman@gmail.com>
2025-11-17 08:23:54 -0600

    Update print statement to use parentheses

--- biskit/data/amber/template_pme_ensemble_amber9/2_md_xx/lamboot_if_needed.py
+++ biskit/data/amber/template_pme_ensemble_amber9/2_md_xx/lamboot_if_needed.py
@@ -7,7 +7,7 @@ lamReturn = commands.getoutput('lamnodes')
 
 if lamReturn[:10] == 10*'-':
     ## LAM not running
-    print "no LAM nodes found -> lamboot"
+    print("no LAM nodes found -> lamboot")
     os.system( 'lamboot' )
 else:
     print "found LAM nodes: ", lamReturn
